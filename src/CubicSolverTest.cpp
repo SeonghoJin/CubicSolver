1810e16804fa70c2eb58cf58df74080f02ab4c57
SeonghoJin <seongho4996@naver.com>
2020-09-25 22:49:04 +0900

    pass test

--- src/CubicSolverTest.cpp
+++ src/CubicSolverTest.cpp
@@ -4,33 +4,65 @@
 #include <bitset>
 #include <list>
 #include "CubicSolver.hpp"
-class Token
-{
+
+
+class Token{
+	public:
+	string name = "1";
+	string toString() const {
+		return name;
+	}
+	Token(string str){
+		name = str;
+	}
+
+	Token(){
+	}
+
+	bool operator<(const Token& t)const{
+		return this->name < t.name;
+	}
+	bool operator!=(const Token& t) const{
+		return name != t.name;
+	}
 };
-class Variable
-{
+
+class Variable{
+	public:
+	string toString(){
+		return "2";
+	}
 };
+
+void ff(set<int>& t){
+	cout << &(*t.begin()) << '\n';
+	int k = 5;
+	t.insert(k);
+	cout << &(++t.begin()) << '\n';
+}
+
+
+
 int main()
 {
 
-    //Example1
-    Token t1;
-    Token t2;
-    Token t3;
-    Variable v1;
-    Variable v2;
-    Variable v3;
-    Variable v4;
-    Variable v5;
-    CubicSolver<Variable, Token> cubic1;
-    cubic1.addSubsetConstraint(&v1, &v5);                // V1 ⊂ V5
-    cubic1.addSubsetConstraint(&v1, &v2);                // V1 ⊂ V2
-    cubic1.addSubsetConstraint(&v3, &v4);                // V3 ⊂ V4
-    cubic1.addConstantConstraint(&t3, &v1);              // T3 ∈ V1
-    cubic1.addConstantConstraint(&t2, &v3);              // T2 ∈ V3
-    cubic1.addConditionalConstraint(&t3, &v2, &v2, &v3); // T3 ∈ V2 -> V2 ⊂ V3
-    cubic1.addConditionalConstraint(&t3, &v3, &v3, &v1); // T3 ∈ V3 -> V3 ⊂ V1
-    cubic1.addConstantConstraint(&t1, &v1);              // T1 ∈ V1
-    cubic1.addConditionalConstraint(&t2, &v3, &v5, &v3); // T2 ∈ V3 -> V5 ⊂ V3
-    cubic1.print();
+  //Example1
+	Token t1("1");
+	Token t2("2");
+	Token t3("3");
+	Token t4("4");
+	Token t5("5");
+	Token t6("6");
+	
+	CubicSolver<Token, Token> cubic1;
+	
+	cubic1.addConstantConstraint(t1,t2);
+	cubic1.addConstantConstraint(t3,t4);
+	cubic1.addConstantConstraint(t5,t6);
+	
+	string str = "1" + t1.toString();
+
+	cubic1.print();
 }
+
+
